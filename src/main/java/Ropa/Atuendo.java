cdf8b72347fb98fd8ab7d65e75d6b91a2332e0a7
AlejandroDeheza <ale.deheza@gmail.com>
2021-04-20 23:31:55 -0300

    Arreglo bug en Atuendo..

--- src/main/java/Ropa/Atuendo.java
+++ src/main/java/Ropa/Atuendo.java
@@ -5,4 +5,12 @@ import java.util.List;
 public class Atuendo {
 
   private List<Prenda> prendas;
+
+  public List<Prenda> getPrendas() {
+    return prendas;
+  }
+
+  public void setPrendas(List<Prenda> prendas) {
+    this.prendas = prendas;
+  }
 }
