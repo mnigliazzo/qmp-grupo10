a7ac3ede1023c0f13051670ffe14e7fac13ad1e0
Matias Nigliazzo <mnigliazzo@frba.utn.edu.ar>
2021-04-25 09:50:20 -0300

    Modifico clase cambiando el tipado de atributos

--- src/main/java/domain/prenda/Prenda.java
+++ src/main/java/domain/prenda/Prenda.java
@@ -4,23 +4,14 @@ import java.util.Objects;
 
 public class Prenda {
 	TipoPrenda tipo;
-	String material; // TODO: Verificar el comportamiento que tendra el Material, esta como string ya que al no tener comportamiento por ahora no pareciera ser necesario darle comportamiento
-	String colorPrimario; // TODO: Verificar el comportamiento que tendra el Color, esta como string ya que al no tener comportamiento por ahora no pareciera ser necesario darle comportamiento
-
-	String colorSecundario; // TODO: Verificar el comportamiento que tendra el Color, esta como string ya que al no tener comportamiento por ahora no pareciera ser necesario darle comportamiento
-
-	public Prenda(TipoPrenda tipo, String material, String colorPrimario) {
+	Material material;
+	Color colorPrimario;
+	Color colorSecundario;
+	public Prenda(TipoPrenda tipo, Material material, Color colorPrimario,Color colorSecundario) {
 		this.tipo = Objects.requireNonNull(tipo, "Tipo no puede ser Null");
 		this.material = Objects.requireNonNull(material, "Material no puede ser Null");
 		this.colorPrimario = Objects.requireNonNull(colorPrimario, "colorPrimario no puede ser Null");
-	}
-
-	public String getColorSecundario() {
-		return colorSecundario;
-	}
-
-	public void setColorSecundario(String colorSecundario) {
-		this.colorSecundario = colorSecundario;
+		this.colorSecundario=colorSecundario;
 	}
 
 	public Categoria getCategoria() {
